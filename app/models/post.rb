f4f1ae714cd699a3eb873334f2b6bf5f4dc9f225
Abraham <abdiaz2018@gmail.com>
2021-05-24 19:38:38 -0700

    commit tree e14ddb526f1dc219662fcddcb1f00042a9193787

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-    include ActiveModel::Validations
-    validates_with TitleValidator
+    validate :clickbaitey?
+    
+    def clickbaitey?
+        if title && !title.match(/Won't Believe|Secret|Guess|Top [0-9]*/)
+            errors.add(:title, "No clickbait!")
+        end
+    end
 end
